47d21c2e27e3e1f52b2b40fe165b1d1d58f94e72
Hey_Nerd <gargharshit765@gmail.com>
2026-08-01 16:53:26 +0530

    Sets Basics

---
+++ python/data_structures/in_build_DS/sets/setsBasic.py
@@ -0,0 +1,6 @@
+#sets are unordered collections of unique elements in python. They are defined using curly braces {} or the set() constructor.
+#Sets can contain elements of different data types, including numbers, strings, and other sets. However, sets cannot contain mutable elements like lists or dictionaries.
+#Sets are unordered, cannot have duplicates, can be nested, and can be accessed using membership testing.
+
+s = {1, 2, 3, 4, 4, 4, 5, 5}  #Creating a set with elements 1, 2, 3, 4, and 5
+print(f"Set s: {s}")
