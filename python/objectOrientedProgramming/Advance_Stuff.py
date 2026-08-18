967a2ae6b7af16aefe9dc4e4b397a729f1900385
Hey_Nerd <gargharshit765@gmail.com>
2026-08-18 23:12:38 +0530

    Python advance Stuff: List, dict, set comprehensions; Lambda functions, map , filter and Modules, next stuff is packages

--- python/objectOrientedProgramming/Advance_Stuff.py
+++ python/objectOrientedProgramming/Advance_Stuff.py
@@ -4,6 +4,12 @@
 #Its tough to understand with text
 
 
+# --------------------------------------
+def new_line():
+    for i in range(25):
+        print("-",end=" ")
+    print("\n")
+# ---------------------------------------
 
 def my_decorator(func):
     def wrapper():
@@ -82,3 +88,115 @@ def info(**kwargs):
         print(f"{i} : {kwargs[i]}")
 
 info(name = "Harshit", age = 23, designation = "AI/ML")
+
+# ----------------------------------------
+new_line()
+# new = [print("-",end=" ") for i in range(15)]
+# ----------------------------------------
+
+# List, Dictionary and set comphrehension
+# - All of these comphrehensions are used to create list, dictionary, and set. but we donot have to use multiple lines of code for loops and if-else statements.
+
+labels = ["Even" if x % 2 == 0 else "Odd" for x in range(5)]    #list comprehension
+print(labels,"\n")
+
+
+# new = [print("-",end=" ") for i in range(15)]                         # for new line easy and simple
+
+
+evens = {x: x * x for x in range(10) if x%2 == 0}           #dictionary comprehension
+print(evens)
+
+print("\n")
+
+unique_even_squares = {x*x for x in range(10) if x % 2 == 0}        #set comprehension
+#{0, 4, 16, 36, 64}
+print(unique_even_squares)
+
+
+print("\n")
+
+print_even = {x for x in range (1,21) if x % 2 == 0}
+print(print_even)
+
+
+new_line()
+
+# ------------------------------------
+
+# lambda functions - A lambda function is an anonymous, inline function defined using the lambda keyword.
+# - Its often used for short, simple functions that are used only once or temporarily
+# - you can have multiple arguments but there will be only one expression.
+
+
+new_addition_using_lambda = lambda a,b : a+b
+print("The sum of a and b is ", new_addition_using_lambda(2,4))
+
+check_even_using_lambda = lambda a : "even" if a % 2 == 0 else "odd"
+print("Your Number is :",check_even_using_lambda(42))
+print("Your Number is :",check_even_using_lambda(11))
+
+
+
+square = lambda x: x**2
+print("Square of your number is: ",square(4))
+
+
+
+new_line()
+
+# ----------------------------
+
+# Map filter and zip
+# - Map is used for applying a function to multiple items.
+# - Takes a list(or any sequences)
+# - Applies the same function to every item in that list
+# - Gives us back a new list(in Python 3, it gives a map object which you can convert to a list)
+
+
+numbers = [1,2,3,4,5]
+doubled = map(lambda x: x*2 , numbers)
+print(list(doubled))
+
+# - Use map() when you want to transform every items in a list.
+# - it doesnot remove or skip items(that's what filter() does). 
+# - you can use it with lambda or normal functions.
+
+# - Filter as the name suggest is used to filter out the stuff.
+# - Takes a list (or other sequences)
+# - Checks each items using a function(a test)
+# - keeps only the items that pass the test (i.e return True)
+
+numbers = [1,2,3,4]
+evens = filter(lambda x : x % 2 == 0, numbers)
+print(list(evens))
+
+
+new_line()
+
+# -------------------------------
+
+# Modules and packages
+# - Module is just a single file containing code and we can use this file code in other file.
+# - A single python file(.py)
+# - Contains functions, variables, or classes
+# - Used to organize and reuse code
+# - python comes with lots of ready to use modules like:
+#       1. math(for math operations)
+#       2. random(for random generating numbers)
+#       3. datetime(for date and time) 
+
+
+import math
+print(math.sqrt(16))
+
+
+import self_maths_module
+print("sum of your numbers is:", self_maths_module.addFromModule(4,6))
+print("product of your number is:", self_maths_module.multiplicationFromModule(4,5))
+
+# or we can also import the whole function like this
+from self_maths_module import addFromModule                         # function imported from module
+print("sum of your numbers is:", self_maths_module.addFromModule(11,54))
+
+
---
+++ python/objectOrientedProgramming/self_maths_module.py
@@ -0,0 +1,5 @@
+def addFromModule(a,b):
+    return a + b
+
+def multiplicationFromModule(a,b):
+    return a * b
